8ba0db8a7277853f2e6e1622d6e69df9668d763b
Pong En-Yu <andrew0911114933@gmail.com>
2021-10-15 14:18:11 +0800

    Update for function call

--- UglyNumber.c
+++ UglyNumber.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+int isUgly(int n);
+
 int main() {
-    int n = 101;
-    bool isUgly = true;
-    if (n < 1)
-	isUgly = false;
-    while (n > 1) {
+    int n = 100;
+    int ugly = isUgly(n);
+    char *str;
+    if (ugly)
+        printf("%d is ugly.\n", n);
+    else 
+        printf("%d is not ugly.\n", n);
+    
+}
+
+int isUgly(int n) {
+    int ugly = 1;
+    if (n <= 1)
+	    ugly = 0;
+    do {
         if (n % 2 == 0)
             n /= 2;
-	else if (n % 3 == 0)
-	    n /= 3;
-	else if(n % 5 == 0)
-	    n /= 5;
-	else {
-	    isUgly = false;
+	    else if (n % 3 == 0)
+	        n /= 3;
+	    else if(n % 5 == 0)
+	        n /= 5;
+	    else {
+	        ugly = 0;
             break;
-	}
-    }
-    printf("%d\n", isUgly);
+	    }
+    } while (n > 1);
+    return ugly;
 }
--- UglyNumber.s
+++ UglyNumber.s
@@ -1,48 +1,58 @@
 .data
-str1: .string "is ugly"
-str2: .string "is not ugly"
+str1: .string " is ugly"
+str2: .string " is not ugly"
+input_num: .word 300                # input number
 
 .text
 main:
-    li a1, 1    # isUgly = 1
-    li a2, 630   # n = 100
-    li a3, 1    # const 1
-    bge a3, a2, not_ugly
-    li a4, 2    # const 2
-    li a5, 3    # const 3
-    li a6, 5    # const 5
-loop:
-    rem a6, a2, a4
-    bne a6, zero, elif_3
-    div a2, a2, a4
-    j   compare
+    lw    s2, input_num                # load the value of input number n
+    add   a0, s2, zero                 # function argument
+    jal   func                         # jump to function
+    add   s3, a1, zero                 # store the return value of func
+    beqz  s3, not_ugly                 # branch when input number is not ugly
+    add   a0, s2, zero                 # print input integer
+    li    a7, 1
+    ecall
+    la    a0, str1                     # print str1
+    li    a7, 4
+    ecall
+    j     end
+not_ugly:
+    add   a0, s2, zero                 # print input integer
+    li    a7, 1                        
+    ecall
+    la    a0, str2                     # print str2
+    li    a7, 4
+    ecall
+end:                                   # end programm
+    li    a7, 10
+    ecall
+         
+func:
+    li    a1, 1                        # return value of func
+loop:                                  # begin of the do-while loop
+    li    t0, 2
+    rem   t4, a0, t0                   # t4 = n % 2
+    bne   t4, zero, elif_3             # if t4 != 0, go to elif_3
+    srli  a0, a0, 1                    # if t4 == 0, n /= 2
+    j     compare                      
 elif_3:
-    rem a6, a2, a5
-    bne a6, zero, elif_5
-    div a2, a2, a5
-    j   compare    
+    li    t0, 3                        # t4 = n % 3
+    rem   t4, a0, t0                   # if t4 != 0, go to elif_5
+    bne   t4, zero, elif_5             # if t4 == 0, n /= 3
+    div   a0, a0, t0
+    j     compare    
 elif_5:
-    rem a6, a2, a6
-    bne a6, zero, else
-    div a2, a2, a6
-    j   compare
+    li    t0, 5                        # t4 = n %= 5
+    rem   t4, a0, t0                   # if t4 != 0, go to else 
+    bne   t4, zero, else               # if t4 == 0, n /= 5
+    div   a0, a0, t0
+    j     compare
 else:
-    li a1, 0
-    j print
-compare:    
-    blt a3, a2, loop
-
-print:
-    beqz a1, not_ugly
-ugly:
-    la a0, str1
-    li a7, 4
-    ecall
-    j end
-not_ugly:    
-    la a0, str2
-    li a7, 4
-    ecall
-end:
-    li a7, 10
-    ecall
+    li    a1, 0                        # n is not ugly
+    j     return                       # break
+compare:
+    li    t0, 1                        
+    blt   t0, a0, loop                 # if n > 1 go to loop
+return:
+    jr    ra
