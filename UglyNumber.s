28e7c2cb314f6c951d4a41f19fb3a824e55ee3da
Pong En-Yu <andrew0911114933@gmail.com>
2021-10-21 11:10:14 +0800

    Add if n >= 1

--- UglyNumber.s
+++ UglyNumber.s
@@ -1,13 +1,13 @@
 .data
-str1: .string " is ugly"
-str2: .string " is not ugly"
+str1: .string " is ugly."
+str2: .string " is not ugly."
 
 .text
 main:
-    li    a5, 210                      # load the value of input number n
+    li    a5, 300                      # load the value of input number n
     add   a0, a5, zero                 # function argument
     sw    a0, 0(sp)                    # store the input number
-    jal   func                         # jump to function is Ugly
+    jal   isUgly                       # jump to function isUgly
     add   a2, a0, zero                 # store the return value of func isUgly
     lw    a0, 0(sp)                    # restore the input number                   
     beqz  a2, not_ugly                 # branch when input number is not ugly
@@ -19,12 +19,18 @@ end:                                   # end programm
     li    a7, 10
     ecall
     
-func:
+isUgly:
     addi  sp, sp, -8                   # make space on stack 
-    sw    ra, 4(sp)
+    sw    ra, 4(sp)                 
     sw    a0, 0(sp)
     add   a5, a0, zero
     li    a0, 1                        # return value of func
+    li    a4, 1
+    bge   a4, a5, less_than_two        # check if input integer <= 1
+    j     loop
+less_than_two:
+    li    a0, 0
+    j     ret    
 loop:                                  # begin of the do-while loop
     li    a2, 2
     rem   a4, a5, a2                   # a4 = n % 2
@@ -40,20 +46,19 @@ elif_3:
 elif_5:
     li    a2, 5                        # a4 = n %= 5
     rem   a4, a5, a2                   # if a4 != 0, go to else 
-    bne   a4, zero, else               # if a4 == 0, n /= 5
+    bne   a4, zero, set_not_ugly       # if a4 == 0, n /= 5
     div   a5, a5, a2
     j     compare
-else:
+set_not_ugly:
     li    a0, 0                        # n is not ugly
-    j     return                       # break
+    j     ret                          # break
 compare:
     li    a3, 1                        
     blt   a3, a5, loop                 # if n > 1 go to loop
-return:
+ret:
     lw    ra, 4(sp)                    # restore ra
     addi  sp, sp, 8                    # restore stack
     jr    ra                           # return to main
-    
 print_ugly:
     li    a7, 1                        # a0 is 1            
     ecall
